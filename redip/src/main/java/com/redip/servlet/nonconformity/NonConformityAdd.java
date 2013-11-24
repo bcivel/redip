778e476d393371ee1dc677f5f4bc2b32bde862c1
bcivel <bcivel@PW7-070177.siege.red>
2013-11-24 22:08:37 +0100

    NonconformityAdd servlet added

--- redip/src/main/java/com/redip/servlet/nonconformity/NonConformityAdd.java
+++ redip/src/main/java/com/redip/servlet/nonconformity/NonConformityAdd.java
@@ -5,8 +5,13 @@
 package com.redip.servlet.nonconformity;
 
 import com.redip.entity.QualityNonconformities;
+import com.redip.entity.QualityNonconformitiesDoc;
+import com.redip.factory.IFactoryQualityNonconformities;
+import com.redip.service.IEmailService;
+import com.redip.service.IQualityNonconformitiesDocService;
 import com.redip.service.IQualityNonconformitiesImpactService;
 import com.redip.service.IQualityNonconformitiesService;
+import java.io.File;
 import org.springframework.context.ApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
@@ -15,6 +20,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import javax.servlet.RequestDispatcher;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileItemFactory;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
 /**
  * @author bcivel
@@ -23,37 +36,144 @@ public class NonConformityAdd extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String application = request.getParameter("Application");
-        String problemTitle = request.getParameter("ProblemTitle");
-        String problemDescription = request.getParameter("ProblemDescription");
-        String startDate = request.getParameter("StartDate");
-        String startTime = request.getParameter("StartTime");
-        String endDate = request.getParameter("EndDate");
-        String endTime = request.getParameter("EndTime");
-        String severity = request.getParameter("Severity");
-        String reproductibility = request.getParameter("Reproductibility");
-        String linkToDoc = request.getParameter("LinkToDoc");
-        String behaviorExpected = request.getParameter("BehaviorExpected");
-        String detection = request.getParameter("Detection");
-        String screenshot = request.getParameter("Screenshot");
+        
+        String application = "";
+        String problemTitle = "";
+        String problemDescription = "";
+        String startDate = "";
+        String startTime = "";
+        String endDate = "";
+        String endTime = "";
+        String severity = "";
+        String reproductibility = "";
+        String linkToDoc = "";
+        String behaviorExpected = "";
+        String detection = "";
+        String screenshot = "";
         String impactOrCost = "";
+        FileItem item = null;
+        
+        if (ServletFileUpload.isMultipartContent(request)) {
+            FileItemFactory factory = new DiskFileItemFactory();
+            ServletFileUpload upload = new ServletFileUpload(factory);
 
+            try {
+                String fileName = null;
+                List items = upload.parseRequest(request);
+                Iterator iterator = items.iterator();
+                File uploadedFile = null;
+                
+                while (iterator.hasNext()) {
+                    item = (FileItem) iterator.next();
+
+                    if (!item.isFormField()) {
+                        fileName = item.getName();
+
+//                        String root = getServletContext().getRealPath("/");
+                        
+                    } else {
+                        String name = item.getFieldName();
+                        if (name.equals("Application")) {
+                            application = item.getString();
+                            System.out.println(application);
+                        }
+                        if (name.equals("ProblemTitle")) {
+                            problemTitle = item.getString();
+                            System.out.println(problemTitle);
+                        }
+                        if (name.equals("ProblemDescription")) {
+                            problemDescription = item.getString();
+                            System.out.println(problemDescription);
+                        }
+                        if (name.equals("StartDate")) {
+                            startDate = item.getString();
+                            System.out.println(startDate);
+                        }
+                        if (name.equals("StartTime")) {
+                            startDate = item.getString();
+                            System.out.println(startDate);
+                        }
+                        if (name.equals("EndDate")) {
+                            endDate = item.getString();
+                            System.out.println(endDate);
+                        }
+                        if (name.equals("EndTime")) {
+                            endTime = item.getString();
+                            System.out.println(endTime);
+                        }
+                        if (name.equals("Severity")) {
+                            severity = item.getString();
+                            System.out.println(severity);
+                        }
+                        if (name.equals("Reproductibility")) {
+                            reproductibility = item.getString();
+                            System.out.println(reproductibility);
+                        }
+                        if (name.equals("BehaviorExpected")) {
+                            behaviorExpected = item.getString();
+                            System.out.println(behaviorExpected);
+                        }
+                        if (name.equals("Detection")) {
+                            detection = item.getString();
+                            System.out.println(detection);
+                        }
+                        if (name.equals("Screenshot")) {
+                            screenshot = item.getString();
+                            System.out.println(screenshot);
+                            System.out.println(screenshot.length());
+                        }
+                    }
+                    
+                    
+                }
+                
         ApplicationContext appContext = WebApplicationContextUtils.getWebApplicationContext(this.getServletContext());
-
+        IQualityNonconformitiesDocService nonconformitiesDocService = appContext.getBean(IQualityNonconformitiesDocService.class);
         IQualityNonconformitiesService nonconformitiesService = appContext.getBean(IQualityNonconformitiesService.class);
         IQualityNonconformitiesImpactService nonconformitiesImpactService = appContext.getBean(IQualityNonconformitiesImpactService.class);
-
-        String str = nonconformitiesService.addNonconformity(problemTitle, problemDescription,
-                severity, reproductibility, linkToDoc, behaviorExpected, detection, startDate, startTime, screenshot);
+        IEmailService emailService = appContext.getBean(IEmailService.class);
+        IFactoryQualityNonconformities factoryQNC = appContext.getBean(IFactoryQualityNonconformities.class);
+        
+        QualityNonconformities nonconformitiestoadd = factoryQNC.create(problemTitle, 
+                problemDescription, severity, reproductibility, linkToDoc, behaviorExpected, detection,
+                startDate, startTime, screenshot);
+        
+        String str = nonconformitiesService.addNonconformity(nonconformitiestoadd);
 
         QualityNonconformities qncCount = nonconformitiesService.getMaxId();
         int id = qncCount.getIdqualitynonconformities();
-
+        nonconformitiestoadd.setIdqualitynonconformities(id);
+        
         String str2 = nonconformitiesImpactService.addNonconformityImpact(id, application,
                 startDate, startTime, endDate, endTime, impactOrCost);
+        
+        String root = "D:/CerberusDocuments/redip/";
+                        File pathFile = new File(root + id);
+                        if (!pathFile.exists()) {
+                            pathFile.mkdirs();
+                        }
 
-        //response.getWriter().print(str.concat("/").concat(str2));
+                        if (!fileName.isEmpty()){
+                        uploadedFile = new File(pathFile + "/" + fileName);
+                        System.out.println(uploadedFile.getAbsolutePath());
+                        item.write(uploadedFile);
+                        }
+        
+        QualityNonconformitiesDoc ncDoc = new QualityNonconformitiesDoc();
+            ncDoc.setIdQualityNonconformities(Integer.valueOf(id));
+            ncDoc.setLinkToDoc(fileName);
+            
+            String addLinktodoc = nonconformitiesDocService.addNonconformityDoc(ncDoc);
+
+        emailService.sendEmailEvent("addNonconformity", nonconformitiestoadd, null, null);
+        
         response.sendRedirect("qualitynonconformities.jsp");
-
+            } catch (FileUploadException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+    }
+        
     }
 }
